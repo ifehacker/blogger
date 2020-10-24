f56cb529fb7c28dd85b7461a23e5fc1a2bba44b8
Ife Oshoba <ife.oshoba@gmail.com>
2020-10-24 21:42:40 +0100

    form based workflow

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
     def index
         @articles = Article.all
     end
@@ -8,7 +9,31 @@ class ArticlesController < ApplicationController
     end
 
     def new 
-    
+        @article = Article.new
     end
 
+    def create 
+        @article = Article.new
+        @article.title = params[:article][:title]
+        @article.body = params[:article][:body]
+        @article.save
+        redirect_to article_path(@article)
+        end
+
+    def destroy 
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    def edit 
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        flash.notice = "You have updated #{@article.title}"
+        redirect_to article_path(@article)
+    end
 end
